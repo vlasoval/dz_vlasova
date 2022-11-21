221ccc04af13e5f103ead5bdc6d2ba2f1ebe0b02
Vlasova Liliya <vlasova.liliya.li@gmail.com>
2022-11-21 13:01:41 +0300

    lk20, 1/2

--- src/orders/admin.py
+++ src/orders/admin.py
@@ -3,6 +3,8 @@ from . import models
 
 class CartAdmin(admin.ModelAdmin):
     list_display = ('pk', 'user')
-
+class BookInCartAdmin (admin.ModelAdmin):
+    list_display = ('pk', 'book', 'cart', 'quantity', 'created_date', 'updated_date')
 
 admin.site.register(models.Cart, CartAdmin)
+admin.site.register(models.BookInCart, BookInCartAdmin)
--- src/orders/models.py
+++ src/orders/models.py
@@ -12,3 +12,25 @@ class Cart(models.Model):
         null=True,
         blank=True,
     )
+
+class BookInCart(models.Model):
+    book = models.ForeignKey(
+        "book.Book",
+        verbose_name='Book in a cart',
+        on_delete=models.PROTECT)
+    cart = models.ForeignKey(
+        "orders.Cart",
+        verbose_name='Cart',
+        related_name='books',
+        on_delete=models.PROTECT)
+    quantity = models.IntegerField(
+        verbose_name='Quantity',
+        default=1)
+    created_date = models.DateTimeField(
+        'Created date',
+        auto_now=False,
+        auto_now_add=True)      
+    updated_date = models.DateTimeField(
+        'Updated date',
+        auto_now=True,
+        auto_now_add=False)  
