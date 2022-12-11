f82751f639382db4e7111b13999db60b191e020a
Vlasova Liliya <vlasova.liliya.li@gmail.com>
2022-12-11 16:35:51 +0300

    currency_session

--- src/home_page/views.py
+++ src/home_page/views.py
@@ -3,20 +3,30 @@ from django.views import generic
 from book import models as b_models
 from book_reference import models
 import requests
+import time
 # Create your views here.
 
 class BaseTemplatePageMixin:
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args,**kwargs)
         context['current_genre_all'] = models.BookGenre.objects.all()
-        headers = {'Accept': 'application/json'}
-        
-        try:
-            r = requests.get('https://www.nbrb.by/api/exrates/rates/431', headers=headers, timeout=1)
-            currency_object = r.json()
-            currency_rate = currency_object['Cur_OfficialRate']
-        except:
-            currency_rate = 2.5
+
+        current_time_in_seconds = time.time()
+        one_day_in_seconds = 60*60*24
+        currency_rate = self.request.session.get('currency_rate', None)
+        currency_rate_updated = self.request.session.get('currency_rate_updated', current_time_in_seconds)
+
+        if not currency_rate or (current_time_in_seconds - currency_rate_updated) > one_day_in_seconds:
+            try:
+                headers = {'Accept': 'application/json'}
+                r = requests.get('https://www.nbrb.by/api/exrates/rates/431', headers=headers, timeout=1)
+                currency_object = r.json()
+                currency_rate = currency_object['Cur_OfficialRate']
+            except:
+                currency_rate = 2.5
+                
+            self.request.session['currency_rate'] = currency_rate
+            self.request.session['currency_rate_updated'] = time.time()
 
         context['currency'] = currency_rate
         return context
